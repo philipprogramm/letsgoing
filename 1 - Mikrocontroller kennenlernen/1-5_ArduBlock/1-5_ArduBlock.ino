3de3d3aa8353e2f3ac7b4967cb18bb9b28094cab
Philipp Stappert <mail@philipprogramm.de>
2020-01-10 15:17:52 +0100

    Solutions book 1 - undebugged
    solutions of the first programming book but not debugged at the moment

---
+++ 1 - Mikrocontroller kennenlernen/1-5_ArduBlock/1-5_ArduBlock.ino
@@ -0,0 +1,15 @@
+// 1.5 ArduBlock
+// Solution by philipprogramm
+// 
+// Pins:
+// none
+
+void setup(){
+    pinMode(13, OUTPUT); // set PIN 13 (onboard led) as output
+}
+void loop(){
+    digitalWrite(13, HIGH); // turn led on
+    delay(1000); // wait 1s
+    digitalWrite(13, LOW); // turn led on
+    delay(1000); // wait 1s
+}
---
+++ 1 - Mikrocontroller kennenlernen/1-7_Uebungsprojekt/1-7_Uebungsprojekt.ino
@@ -0,0 +1,72 @@
+// 1.7 Übungsprojekt
+// Solution by philipprogramm
+// 
+// Pins
+// none
+
+void setup(){
+    pinMode(13, OUTPUT); // set pin 13 (onboard led) as output
+}
+void loop(){
+    // morse S
+    //  Blink 1
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+    //  Blink 2
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+    //  Blink 3
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+
+    // morse O
+    //  Blink 1
+    digitalWrite(13, HIGH); // turn led on
+    delay(900); // wait 900 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(900); // wait 900 ms
+
+    //  Blink 2
+    digitalWrite(13, HIGH); // turn led on
+    delay(900); // wait 900 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(900); // wait 900 ms
+
+    //  Blink 3
+    digitalWrite(13, HIGH); // turn led on
+    delay(900); // wait 900 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(900); // wait 900 ms
+
+
+    // morse S
+    //  Blink 1
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+    //  Blink 2
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+    //  Blink 3
+    digitalWrite(13, HIGH); // turn led on
+    delay(300); // wait 300 ms
+    digitalWrite(13, LOW); // turn led on
+    delay(300); // wait 300 ms
+
+    // wait for next word
+    delay(1200); // wait 1200ms
+}
