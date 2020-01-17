53f34e55ef8da0419daeac28e66d763478a05485
Philipp Stappert <mail@philipprogramm.de>
2020-01-17 16:00:41 +0100

    heft 3 finished with comments

--- 3 - Analoge Signale und Variablen/3-7_Uebungsprojekt/3-7_Uebungsprojekt.ino
+++ 3 - Analoge Signale und Variablen/3-7_Uebungsprojekt/3-7_Uebungsprojekt.ino
@@ -7,6 +7,7 @@
 // 5: rgb-module pin 2 (red)
 // 3: rgb-module pin 3 (green)
 // Note: A stands for analog pin of the arduino
+
 int farbe1 = 0;
 int farbe2 = 255;
 
@@ -17,8 +18,8 @@ void setup() {
 }
 
 void loop(){
-    int farbe1 = map(analogRead(A0), 0, 1023, 0, 255);
-    int farbe2 = 255 - map(analogRead(A0), 0, 1023, 0, 255);
-    analogWrite(5, farbe1);
+    int farbe1 = map(analogRead(A0), 0, 1023, 0, 255); // map poti input into variable farbe1
+    int farbe2 = 255 - map(analogRead(A0), 0, 1023, 0, 255); // map poti into variable but inverted
+    analogWrite(5, farbe1); // write color-values to analog
     analogWrite(6, farbe2);
 }
